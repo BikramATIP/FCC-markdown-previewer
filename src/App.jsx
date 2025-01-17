68c3bb08dae3e71415c7a9bcf892bd4ad2185ce1
Bikram Thapa <bikramt755@gmail>
2025-01-17 13:41:27 +1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+
+function App() {
+  
+
+  return (
+    <>
+   <div className="editor-container">
+  <textarea id="editor">
+
+  </textarea>
+   </div>
+   <div className="previewer-container" id="preview">
+
+   </div>
+    </>
+  )
+}
+
+export default App
